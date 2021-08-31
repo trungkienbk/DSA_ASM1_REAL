7219de8ddf46f3926b20d0324c11dc7692197f50
trungkien04102002 <kien.ha04102002@hcmut.edu.vn>
2021-08-31 21:43:31 +0700

    Test_Hoang

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -1,13 +1,15 @@
 #include "SymbolTable.h"
 using namespace std;
 void SymbolTable::run(string filename){
+    string line,method,name,attribue;
+    int *arr = new int[2]{0};
+    SymbolTable list;
     fstream input_file;
-    string line;
     int count = 0;
     input_file.open(filename);
     if(input_file){
         while(std::getline(input_file,line)){
-            if( count == 2){
+            if( count == 5){
                 return;
             }
             cout<<line<<endl;
@@ -19,3 +21,40 @@ void SymbolTable::run(string filename){
     }
     input_file.close();
 }
+// false -> thao tac dc
+// true -> ko thao tac dc
+bool SymbolTable::contains(SymbolTable &list, Symbol symbol,int *arr) {
+    bool flag = true;
+    SymbolNode* head=list.head();
+    while(head!= nullptr){
+        if(head->data.name_sb == symbol.name_sb ){
+            flag = false;
+            break;
+        }
+        head=head->next;
+    }
+    if(flag) return false;
+    else {
+        SymbolNode* phead=list.head();
+        while(phead!= nullptr){
+            if(phead->data.name_sb == symbol.name_sb && arr[0] > symbol.scope_sb[0] ){
+                return false;
+            }
+            phead = phead->next;
+        }
+    }
+    return true;
+}
+/*bool contains (SLinkedList& list, Soldier soldier){
+    //Check if array contains soldier
+    SoldierNode* head=list.head;
+    while(head!=nullptr){
+        if((head->data.ID== soldier.ID) && (head->data.HP==soldier.HP) && (head->data.isSpecial==soldier.isSpecial)){
+            return true;
+        }
+        head=head->next;
+    }
+    return false;
+}*/
+
+
--- SymbolTable.h
+++ SymbolTable.h
@@ -1,11 +1,59 @@
 #ifndef SYMBOLTABLE_H
 #define SYMBOLTABLE_H
 #include "main.h"
-
-class SymbolTable
-{
+using namespace std;
+class Symbol {
 public:
-    SymbolTable() {}
+    int scope_sb[2] ={0,0};
+    string name_sb;
+    string type_sb;
+    string value_sb;
+public:
+    Symbol(){};
+    //Symbol(const string &nameSb, const string &typeSb) : name_sb(nameSb), type_sb(typeSb) {}; // Constructor
+    Symbol(string name,string type){
+        name_sb =name;
+        type_sb =type;
+    }
+    bool valid_Symbol(string value_sb,string type_sb);
+    void setScope(int arr[]) {
+        scope_sb[0]=arr[0];
+        scope_sb[1]=arr[1];
+    }
+    void setNameSb(const string &nameSb) {
+        name_sb = nameSb;
+    }
+    void setTypeSb(const string &typeSb) {
+        type_sb = typeSb;
+    }
+    void setValueSb(const string &valueSb) {
+        value_sb = valueSb;
+    }
+    bool validType(string name,string value);      // check xem dung loai hay ko ---> TypeMismatch
+};
+class SymbolNode{
+public :
+
+    SymbolNode() {
+        SymbolNode* next=nullptr;
+    }
+    Symbol data;
+    SymbolNode* next;
+};
+class SymbolTable{
+public:
+    //  SymbolTable() {};
     void run(string filename);
+    SymbolTable() {p_head = NULL;};
+    ~SymbolTable() {};
+
+    bool contains(SymbolTable& list,Symbol symbol,int *arr); // check xem da ton tai hay chua --> Undeclared va Redeclared
+    SymbolNode* head() { return p_head; };
+public:
+    SymbolNode* p_head; // Con trỏ pHead trỏ vào phần tử đầu tiên.
 };
 #endif
+
+
+
+
