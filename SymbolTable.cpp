2820bcfaa6804b157ee0ca82e40898e246be1c6a
trungkien04102002 <kien.ha04102002@hcmut.edu.vn>
2021-09-03 15:47:26 +0700

    Xong INSERT

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -1,9 +1,6 @@
-/*#include "SymbolTable.h"
-#include <sstream>;
+#include "SymbolTable.h"
+#include <sstream>
 using namespace std;
-int global_scope = 0;
-int row = 0;
-SymbolTable list;
 int convertInt(string method ){
     if(method == "INSERT") return 1;
     if(method == "LOOKUP") return 2;
@@ -14,48 +11,58 @@ int convertInt(string method ){
     return 7;
 }
 void SymbolTable::run(string filename)
-{   Symbol symbol("x","Number");
-    Symbol symbol1("y","Number");
-    Symbol symbol2("z","Number");
-    Symbol symbol3("t","Number");
-    add_Symbol(list,symbol);
-    add_Symbol(list,symbol1);
-    add_Symbol(list,symbol2);
-    add_Symbol(list,symbol3);
-    pair<int,int> check;
-    string lineInput,method,name,var;
+{
+    int global_scope = 0;
+    int row = 0;
+    SymbolTable list;
+    string ins,method,name,var;
     fstream input;
     input.open(filename);
     if(input){
-        while(std::getline(input,lineInput)){
-            istringstream(lineInput)>>method>>name>>var;
+        while(std::getline(input,ins)){
+            row++;
+            istringstream(ins)>>method>>name>>var; // var  = type or value
             //cout<<lineInput<<"|| Method :"<<method<<" || Name :"<<name<<"|| Var : "<<var<<endl;
            int numberMethod = convertInt(method);
            //Insert: 1 Lookup:2 Assign:3 Print:4 RPrint:5 Begin:6 End:7
            switch(numberMethod){
                case 1:
-               //    cout<<"Ham 1"<<endl;
+                   if(!containsName(list,name)){
+                       Symbol symbol(name,var);
+                       symbol.scope.emplace_back(row,global_scope);
+                       addSymbol(list,symbol);
+                       cout<<"success"<<endl;
+                   } else {
+                       if(!containsScope(list,name,global_scope)){
+                           Node* phead=head;
+                           while(phead!= nullptr){
+                               if(phead->data.name == name ){
+                                   break;
+                               }
+                               phead=phead->next;
+                           }
+                           phead->data.value=var;
+                           phead->data.scope.emplace_back(row,global_scope);
+                           cout<<"success"<<endl;
+                       }
+                       else throw Redeclared(ins);
+                   }
                    break;
                case 2:
-                  // cout<<"Ham 2"<<endl;
                    break;
                case 3:
-                 //  cout<<"Ham 3"<<endl;
                    break;
                case 4:
-                 //  cout<<"Ham 4"<<endl;
                    break;
                case 5:
-                  // cout<<"Ham 5"<<endl;
                     break;
                case 6:
-                   row++;
-                //   cout<<"Ham 6"<<endl;
+                   global_scope++;
                    break;
                default:
-                   if(row == 0) throw UnknownBlock();
-                   row--;
-                  // cout<<"Ham 7"<<endl;
+                   if(global_scope == 0) throw UnknownBlock();
+                   popScope(list,global_scope);
+                   global_scope--;
                    break;
            }
         }
@@ -63,118 +70,69 @@ void SymbolTable::run(string filename)
     else {
         cout<<filename<<" not exist";
     }
+    input.close();
 }
 
-void SymbolTable::add_Symbol(SymbolTable &list, Symbol symbol) {
-    if( list.head == nullptr  ) {
-        Node *head=list.head;
+void SymbolTable::addSymbol(SymbolTable &list, Symbol symbol) {
+    if( head == nullptr ) {
         Node *temp=new Node;
         temp->data= symbol;
-        temp->next = list.head;
+        temp->next = head;
         head = temp;
-        cout<<"success with nullptr"<<endl;
     } else {
-        Node *head = list.head;
-        while(head->next != nullptr){
-            head=head->next;
+        Node *phead =head;
+        while(phead->next != nullptr){
+            phead=phead->next;
         }
         Node *temp = new Node;
         temp->data=symbol;
-        temp->next = head ->next;
-        head->next = temp;
-        cout<<"success"<<endl;
+        temp->next = phead ->next;
+        phead->next = temp;
     }
 }
-void SymbolTable::print_list(SymbolTable &list) {
-    if(list.head == nullptr) cout<<"No elements";
+/*void SymbolTable::printlist(SymbolTable &list) {
+    if(head == nullptr) cout<<"No elements";
     else {
-        Node *temp = list.head;
+        Node *temp = head;
         while(temp!= nullptr){
             cout<<temp->data.name<<" "<<temp->data.type<<" "<<temp->data.value<<endl;
             temp = temp->next;
         }
     }
-}
-*/
-#include "SymbolTable.h"
-using namespace std;
-void SymbolTable::run(string filename){
-    SymbolTable list;
-    Symbol symbol("x","Number");
-    Symbol symbol1("y","Number");
-    Symbol symbol2("z","Number");
-    Symbol symbol3("t","Number");
-    add_Symbol(list,symbol);
-    add_Symbol(list,symbol1);
-    add_Symbol(list,symbol2);
-    add_Symbol(list,symbol3);
-    print_list(list);
-    string line,method,name,attribue;
-    int *arr = new int[2]{0};
-    fstream input_file;
-    int count = 0;
-    input_file.open(filename);
-    if(input_file){
-        while(std::getline(input_file,line)){
-            if( count == 5){
-                return;
-            }
-            cout<<line<<endl;
-            count++;
-        }
-    }
-    else {
-        cout<<filename<< " not exist";
-    }
-    input_file.close();
-}
-// false -> thao tac dc
-// true -> ko thao tac dc
-// if contains == true ,thi goi ham kia.
-bool SymbolTable::contains(SymbolTable &list, Symbol symbol,int *arr) {
-    SymbolNode* phead=head;
+}*/
+// False -> Them duoc , True -> Khong them duoc
+bool SymbolTable::containsName(SymbolTable &list, string name) {
+    if(head == nullptr) return false;
+    Node* phead=head;
     while(phead!= nullptr){
-        if(phead->data.name_sb == symbol.name_sb ){
+        if(phead->data.name == name ){
             return true;
         }
-        head=head->next;
+        phead=phead->next;
     }
     return false;
 }
 
-bool SymbolTable::contains_scope(SymbolTable &list, Symbol symbol, int *arr) {
+bool SymbolTable::containsScope(SymbolTable &list,string name, int scope) {
+    Node* phead=head;
+    while(phead!= nullptr){
+        if(phead->data.name == name ){
+            break;
+        }
+        phead=phead->next;
+    }
+    if(phead->data.scope.back().second == scope){
+        return true;
+    }
     return false;
 }
 
-void SymbolTable::add_Symbol(SymbolTable &list, Symbol symbol) {
-    if( head == nullptr  ) {
-        SymbolNode *temp=new SymbolNode;
-        temp->data= symbol;
-        temp->next = head;
-        head = temp;
-        cout<<"success with nullptr"<<endl;
-    } else {
-        SymbolNode *phead = head;
-        while(phead->next != nullptr){
-            phead=phead->next;
+void SymbolTable::popScope(SymbolTable &list, int scope) {
+    Node* phead=head;
+    while(phead!= nullptr){
+        if(phead->data.scope.back().second == scope){
+            phead->data.scope.pop_back();
         }
-        SymbolNode *temp = new SymbolNode;
-        temp->data=symbol;
-        temp->next = phead ->next;
-        phead->next = temp;
-        cout<<"success"<<endl;
-    }
-
-}
-
-void SymbolTable::print_list(SymbolTable &list) {
-    if(head == nullptr) cout<<"No elements";
-    else {
-        SymbolNode *temp = head;
-        while(temp!= nullptr){
-            cout<<temp->data.name_sb<<" "<<temp->data.type_sb<<" "<<temp->data.value_sb<<endl;
-            cout<<"BEGIN : "<<temp->data.scope_sb[0]<<" & END : "<<temp->data.scope_sb[1]<<endl;
-            temp = temp->next;
-        }
+        phead=phead->next;
     }
 }
--- SymbolTable.h
+++ SymbolTable.h
@@ -1,6 +1,7 @@
-/*#ifndef SYMBOLTABLE_H
+#ifndef SYMBOLTABLE_H
 #define SYMBOLTABLE_H
 #include "main.h"
+using namespace std;
 class Symbol{
 public:
     vector<pair<int,int>> scope;
@@ -9,98 +10,47 @@ public:
     string value;
 public:
     Symbol(){};
-    Symbol(string name,string type){
-        this->name =name;
-        this->name =type;
+    ~Symbol() {
     }
-    void setName(const string &name) {
+
+    Symbol(const string &name, const string &type) : name(name), type(type) {}
+    /*void setName(const string &name) {
         Symbol::name = name;
     }
     void setType(const string &type) {
         Symbol::type = type;
     }
+     */
 };
 class Node{
 public:
-    Node(){
-        Node* next = nullptr;
+   /* Node(){
+    }*/
+
+    ~Node() {
+        delete next;
     }
+
 public:
     Symbol data;
     Node* next;
 };
+//Node *head;
 class SymbolTable
 {
 public:
-    //SymbolTable() {}
-    Node *head;
+    Node *head =nullptr;
     SymbolTable(){
         head = nullptr;
     }
-    ~SymbolTable() {};
-    void add_Symbol(SymbolTable& list,Symbol symbol);
-    void run(string filename);
-    void print_list(SymbolTable& list);
-};
-#endif
- */
-#ifndef SYMBOLTABLE_H
-#define SYMBOLTABLE_H
-#include "main.h"
-using namespace std;
-class Symbol {
-public:
-    int scope_sb[2] ={0,0};
-    string name_sb;
-    string type_sb;
-    string value_sb;
-public:
-    Symbol(){};
-    //Symbol(const string &nameSb, const string &typeSb) : name_sb(nameSb), type_sb(typeSb) {}; // Constructor
-    Symbol(string name,string type){
-        name_sb =name;
-        type_sb =type;
-    }
-    bool valid_Symbol(string value_sb,string type_sb);
-    void setScope(int arr[]) {
-        scope_sb[0]=arr[0];
-        scope_sb[1]=arr[1];
-    }
-    void setNameSb(const string &nameSb) {
-        name_sb = nameSb;
-    }
-    void setTypeSb(const string &typeSb) {
-        type_sb = typeSb;
-    }
-    void setValueSb(const string &valueSb) {
-        value_sb = valueSb;
-    }
-    void showInfo()
-    {
-        std::cout << "  Name = " << name_sb.c_str();
-        std::cout << "  Type = " << type_sb.c_str();
-        cout<<endl;
+    ~SymbolTable() {
+        delete head;
     };
-    bool validType(string name,string value);      // check xem dung loai hay ko ---> TypeMismatch
-};
-class SymbolNode{
-public :
-    Symbol data;
-    SymbolNode* next;
-};
-SymbolNode *head;
-class SymbolTable{
-public:
+    bool containsName(SymbolTable& list,string name);
+    bool containsScope(SymbolTable& list,string name,int scope);
     void run(string filename);
-    SymbolTable() {head = NULL;};
-    ~SymbolTable() {};
-    bool contains(SymbolTable& list,Symbol symbol,int *arr);
-    bool contains_scope(SymbolTable& list,Symbol symbol,int *arr); // check xem da ton tai hay chua --> Undeclared va Redeclared
-    void add_Symbol(SymbolTable& list,Symbol symbol);
-    void print_list(SymbolTable& list);
-    // Con trỏ pHead trỏ vào phần tử đầu tiên.
+    void addSymbol(SymbolTable &list, Symbol symbol);
+  //  void printlist(SymbolTable& list);
+    void popScope(SymbolTable &list,int scope);
 };
-bool Symbol::valid_Symbol(string value_sb, string type_sb) {
-    return false;
-}
 #endif
